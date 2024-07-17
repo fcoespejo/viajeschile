9f6bd1e27f7cf62022fc50818fb3429655825977
Francisco Espejo <fcolinux@gmail.com>
2024-07-17 14:57:36 -0400

    creación carrusel bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+    // smoth scroll
+        $("a").click(function(event) {
+            if (this.hash !== "") {
+                event.preventDefault();
+    
+                var hash = this.hash;
+    
+                $("html, body").animate ({
+                    scrollTop: $(hash).offset().top
+                }, 1000, function() {
+                    window.location.hash = gato;
+                });
+            }
+    
+        });
+})   
