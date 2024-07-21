0d26814710b35240204d8eba232beb2ed8315cf5
Francisco Espejo <fcolinux@gmail.com>
2024-07-21 16:46:38 -0400

    estilo al menu de barra de navegación y efecto al pasar mouse sobre cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,10 +8,25 @@ $(function() {
     
                 $("html, body").animate ({
                     scrollTop: $(hash).offset().top
-                }, 1000, function() {
+                }, 600, function() {
                     window.location.hash = hash;
                 });
             }
     
         });
+})
+
+
+// tooltip en iconos de rrss de footer
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+  });
+
+// aclara cards al deslizar mouse sobre cada una
+$(document).ready(function(){  
+    $(".card").mouseenter(function() {  
+        $(this).css({  
+            opacity: '1.0', 
+        })
+    });  
 })
